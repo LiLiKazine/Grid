b7b4b4a4fbfcadd6291daeee176e4d3bef46d434
LiLiKazine <lilikazine@gmail.com>
2020-01-24 13:19:39 +0800

    grid

---
+++ Grid/GridMask.swift
@@ -0,0 +1,21 @@
+//
+//  GridMask.swift
+//  Grid
+//
+//  Created by LiLi Kazine on 2020/1/24.
+//  Copyright © 2020 LiLi Kazine. All rights reserved.
+//
+
+import UIKit
+
+class GridMask: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
